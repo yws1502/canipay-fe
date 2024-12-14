f73aa15e9a963c4e85e973bae861ef044ce0afd6
yws1502 <yws1502@naver.com>
2024-12-14 22:58:27 +0900

    config: svg webpack 설정 #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
       <main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
-        <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
         <ol className='list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left'>
           <li className='mb-2'>
             Get started by editing{' '}
@@ -18,11 +15,10 @@ export default function Home() {
 
         <div className='flex flex-col items-center gap-4 sm:flex-row'>
           <a
-            className='flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]'
+            className='bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]'
             href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
             target='_blank'
             rel='noopener noreferrer'>
-            <Image className='dark:invert' src='/vercel.svg' alt='Vercel logomark' width={20} height={20} />
             Deploy now
           </a>
           <a
@@ -40,7 +36,6 @@ export default function Home() {
           href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'>
-          <Image aria-hidden src='/file.svg' alt='File icon' width={16} height={16} />
           Learn
         </a>
         <a
@@ -48,7 +43,6 @@ export default function Home() {
           href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'>
-          <Image aria-hidden src='/window.svg' alt='Window icon' width={16} height={16} />
           Examples
         </a>
         <a
@@ -56,7 +50,6 @@ export default function Home() {
           href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'>
-          <Image aria-hidden src='/globe.svg' alt='Globe icon' width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
