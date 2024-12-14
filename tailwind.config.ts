67ea272395c46228e4e4366f9f0a1832e6efa047
yws1502 <yws1502@naver.com>
2024-12-14 15:24:28 +0900

    style: color 설정 #1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        primary: '#029CFD',
+        secondary: '#A7E28C',
+        tertiary: '#FE4685',
+        quaternary: '#FEAE00',
+        white: '#FFFFFF',
+        red: '#D01E1E',
+        gray: {
+          '50': '#F9FAFB',
+          '100': '#F3F4F6',
+          '200': '#E5E7EB',
+          '300': '#D1D5DB',
+          '400': '#9CA3AF',
+          '500': '#6B7280',
+          '600': '#4B5563',
+          '700': '#374151',
+          '800': '#1F2937',
+          '900': '#111827',
+        },
       },
     },
   },
