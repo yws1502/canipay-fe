21fc5ffa95c9435f19b715b10080277197d0845f
yws1502 <yws1502@naver.com>
2024-12-30 00:15:36 +0900

    style: 정의 폰트 설정 line height 100% 설정 #7

--- tailwind/typography.ts
+++ tailwind/typography.ts
@@ -18,7 +18,7 @@ const typography: ResolvableTo<
   'heading-1': [
     '1.625rem', // 26px / 16px = 1.625rem
     {
-      lineHeight: '2.375rem', // 38px / 16px = 2.375rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '700',
     },
@@ -26,7 +26,7 @@ const typography: ResolvableTo<
   'heading-2': [
     '1.375rem', // 22px / 16px = 1.375rem
     {
-      lineHeight: '2rem', // 32px / 16px = 2rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '700',
     },
@@ -34,7 +34,7 @@ const typography: ResolvableTo<
   'heading-3': [
     '1.125rem', // 18px / 16px = 1.125rem
     {
-      lineHeight: '1.625rem', // 26px / 16px = 1.625rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '700',
     },
@@ -42,7 +42,7 @@ const typography: ResolvableTo<
   'body-1': [
     '1rem', // 16px / 16px = 1rem
     {
-      lineHeight: '1.4375rem', // 23px / 16px = 1.4375rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '400',
     },
@@ -50,7 +50,7 @@ const typography: ResolvableTo<
   'body-2': [
     '0.875rem', // 14px / 16px = 0.875rem
     {
-      lineHeight: '1.25rem', // 20px / 16px = 1.25rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '400',
     },
@@ -58,7 +58,7 @@ const typography: ResolvableTo<
   'caption-1': [
     '0.75rem', // 12px / 16px = 0.75rem
     {
-      lineHeight: '1.0625rem', // 17px / 16px = 1.0625rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '400',
     },
@@ -66,7 +66,7 @@ const typography: ResolvableTo<
   'caption-2': [
     '0.625rem', // 10px / 16px = 0.625rem
     {
-      lineHeight: '0.875rem', // 14px / 16px = 0.875rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '300',
     },
@@ -74,7 +74,7 @@ const typography: ResolvableTo<
   'error-1': [
     '0.875rem', // 14px / 16px = 0.875rem
     {
-      lineHeight: '1.25rem', // 20px / 16px = 1.25rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '400',
     },
@@ -82,7 +82,7 @@ const typography: ResolvableTo<
   'error-2': [
     '0.75rem', // 12px / 16px = 0.75rem
     {
-      lineHeight: '0.875rem', // 14px / 16px = 0.875rem
+      lineHeight: '100%',
       letterSpacing: '0',
       fontWeight: '300',
     },
