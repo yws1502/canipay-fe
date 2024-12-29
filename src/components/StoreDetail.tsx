c99b31db7232d00347ebdbc0ab89a54c2a4b3601
yws1502 <yws1502@naver.com>
2024-12-30 01:44:47 +0900

    feat: 매장 상세 클릭 이벤트 추가 #7

--- src/components/StoreDetail.tsx
+++ src/components/StoreDetail.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useParams, useSearchParams } from 'next/navigation';
-import React from 'react';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
+import React, { useState } from 'react';
 import CloseIcon from '@/assets/icons/close.svg';
 import CopyIcon from '@/assets/icons/copy.svg';
 import NaverIcon from '@/assets/icons/naver.svg';
+import { NAVER_MAP_URL } from '@/constants/env';
+import { EXCEPTION_MESSAGE } from '@/constants/error';
 import { QUERY_STRING } from '@/constants/page';
 import { useStoreInfiniteQuery } from '@/hooks/useTMap';
 import Button from './common/buttons/Button';
@@ -14,35 +16,72 @@ function StoreDetail() {
   const params = useParams<{ store: string }>();
   const searchParams = useSearchParams();
   const searchKeyword = searchParams.get(QUERY_STRING.search) ?? '';
+  const router = useRouter();
+
+  const [isCopied, setIsCopied] = useState(false);
 
   const { data: storeList } = useStoreInfiniteQuery(searchKeyword);
 
+  const handleOpenNaver = (item: string) => {
+    if (NAVER_MAP_URL === '') throw new Error(EXCEPTION_MESSAGE.environmentNotSet('NAVER_MAP_URL'));
+    window.open(`${NAVER_MAP_URL}/${item}`, '_blank', 'noopener,noreferrer');
+  };
+
+  const handleCopyAddress = (address: string) => {
+    navigator.clipboard
+      .writeText(address)
+      .then(() => {
+        setIsCopied(true);
+
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1500);
+      })
+      .catch(console.error);
+  };
+
+  // TODO: 더 좋은 방법에 대해 생각 필요
+  const targetStore = storeList.find((store) => store.id === params.store);
+
   return (
     <section className='fixed left-1/2 top-[30%] z-30 w-[90%] -translate-x-1/2 rounded-sm bg-white p-4 shadow-500'>
-      <div className='mb-5'>
-        <div className='mb-2 flex items-start justify-between'>
-          <header>
-            <h3 className='text-heading-3'>식당 이름</h3>
-            <span className='text-caption-1'>카테고리</span>
+      <div className='relative mb-5'>
+        <div className='mb-3 flex items-start justify-between'>
+          <header className='flex-1 overflow-auto'>
+            <h3 className='truncate text-heading-3' title={targetStore?.name}>
+              {targetStore?.name}
+            </h3>
+            <span className='text-caption-1'>{targetStore?.category}</span>
           </header>
           <div className='flex gap-2'>
-            <TextButton>
+            <TextButton
+              onClick={() => handleOpenNaver(`${targetStore?.address} ${targetStore?.name}`)}
+            >
               <NaverIcon className='mr-1' width={16} height={16} />
               <i className='hidden'>네이버로</i> 열기
             </TextButton>
-            <button type='button' className='hover:opacity-80 active:opacity-60'>
+            <button
+              type='button'
+              className='hover:opacity-80 active:opacity-60'
+              onClick={() => router.back()}
+            >
               <CloseIcon className='fill-gray-500' width={16} height={16} />
             </button>
           </div>
         </div>
-        <TextButton>
+        <TextButton onClick={() => handleCopyAddress(targetStore?.address ?? '')}>
           <CopyIcon className='fill-gray-500' width={16} height={16} />
-          서울 광진구 자양로 165 1층
+          {targetStore?.address}
         </TextButton>
+        <p
+          className={`${isCopied ? 'opacity-100' : 'opacity-0'} absolute bottom-[-28px] left-0 rounded-md bg-white px-1.5 py-1 text-caption-1 shadow-500 duration-700 ease-in-out`}
+        >
+          복사 완료!
+        </p>
       </div>
       <div>
         <span className='text-body-2 text-gray-950'>등록하기</span>
-        <div className='mt-3 flex justify-center gap-3'>
+        <div className='mt-4 flex justify-center gap-3'>
           <Button>결제 가능</Button>
           <Button color='red'>결제 불가능</Button>
         </div>
