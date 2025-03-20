cde1016bb95478def99f49ef9f467d8a45ca1c2e
yws1502 <yws1502@naver.com>
2025-03-20 22:26:11 +0900

    feat: 좋아요 초기화 로직 추가 #38

--- src/components/MapLayout.tsx
+++ src/components/MapLayout.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLikedStores } from '@/stores/useLikedStores';
 import NavigationBar from './NavigationBar';
 import MapView from './maps/MapView';
 
 const ResponsiveUi = dynamic(() => import('@/components/ResponsiveUi'), { ssr: false });
 
 function MapLayout() {
+  const { dailyUpdate } = useLikedStores();
+
+  useEffect(() => {
+    dailyUpdate();
+  }, []);
+
   return (
     <>
       <MapView />
